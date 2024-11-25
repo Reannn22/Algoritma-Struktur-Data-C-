33d7ea0f1b469ceef3cfa48e7945480802a130f8
Reyhan Capri Moraga <reyhan.123140022@student.itera.ac.id>
2024-11-25 21:10:56 +0700

    Update Tree C++.cpp

--- Tree C++.cpp
+++ Tree C++.cpp
@@ -10,45 +10,44 @@ struct Node{
 
 nAddress root;
 
-nAddress alloc(infotype x){
+nAddress alloc(infotype X){
     nAddress newNode;
     newNode = new Node();
-    newNode -> info = x;
-    newNode -> left = NULL;
-    newNode -> right = NULL;
-    newNode -> parent = NULL;
+    newNode->info = X;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    newNode->parent = NULL;
     return newNode;
 }
 
 void create(infotype info){ 
-    if (root!=NULL){
-        cout << endl << "Can't make root!!!" << endl; 
+    if (root != NULL){
+        cout << endl << "Tidak bisa membuat root" << endl; 
     }
     else{
         root = alloc(info);
-        cout << endl << "Root added" << endl;
+        cout << endl << "Root ditambah" << endl;
     }
 }
 
-nAddress insertLeft (infotype info, nAddress nodeParent){
-
+nAddress insertLeft(infotype info, nAddress nodeParent){
     if (nodeParent->left != NULL){
-        cout << endl << "Failed to insert left!!!" << endl;
+        cout << endl << "Gagal menambah insert left" << endl;
         return NULL;
     }
     else{
         nAddress newNode;
         newNode = alloc(info);
-        nodeParent->left = newNode ; 
+        nodeParent->left = newNode; 
         newNode->parent = nodeParent; 
-        cout << info << " added as left child of " << nodeParent->info << endl;
+        cout << info << " ditambahkan sebagai anak kiri dari " << nodeParent->info << endl;
         return newNode;
     }
 }
 
-nAddress insertRight (infotype info, nAddress nodeParent){
-    if(nodeParent->right != NULL){
-        cout << endl << "Failed to insert right!!!" << endl;
+nAddress insertRight(infotype info, nAddress nodeParent){
+    if (nodeParent->right != NULL){
+        cout << endl << "Gagal menambah insert right" << endl;
         return NULL;
     }
     else{
@@ -56,21 +55,32 @@ nAddress insertRight (infotype info, nAddress nodeParent){
         newNode = alloc(info);
         nodeParent->right = newNode;
         newNode->parent = nodeParent;
-        cout << info << " added as right child of " << nodeParent->info << endl;
+        cout << info << " ditambahkan sebagai anak kanan dari " << nodeParent->info << endl;
         return newNode;
     }
 }
 
-void print(nAddress P = root){
-    if (!root){
-        cout << "No tree inside!!!" << endl;
+void preOrder(nAddress P){
+    if (P != NULL){
+        cout << P->info << " ";
+        preOrder(P->left);
+        preOrder(P->right);
     }
-    else {
-        if (P!=NULL){
-            cout << P->info << " ";
-            print(P->left); 
-            print(P->right); 
-        }
+}
+
+void inOrder(nAddress P){
+    if (P != NULL){
+        inOrder(P->left);
+        cout << P->info << " ";
+        inOrder(P->right);
+    }
+}
+
+void postOrder(nAddress P){
+    if (P != NULL){
+        postOrder(P->left);
+        postOrder(P->right);
+        cout << P->info << " ";
     }
 }
 
@@ -81,28 +91,43 @@ void clear(nAddress P){
     else {
         clear(P->left);   
         clear(P->right); 
-
-        cout << endl << "Deleting node : " << P->info;
+        cout << endl << "Menghapus node: " << P->info;
         delete P;
     }
 }
 
 void deleteTree(){
     clear(root);
-    cout << endl << "Tree deleted" << endl;
+    cout << endl << "Tree dihapus" << endl;
 }
 
 int main(){
     nAddress node_0, node_1, node_2, node_3, node_4, node_5, node_6, node_7, node_8 ,node_9;
+
     create(1);
     node_2 = insertLeft(2, root);
-    node_3 = insertRight(1, root);
-    node_4 = insertLeft(1, node_2);
-    node_5 = insertRight(4, node_2);
-    node_6 = insertRight(0, node_3);
-    node_7 = insertRight(0, node_5);
-    node_8 = insertLeft(9, node_6);
-    node_9 = insertRight(8, node_6);
-    cout << endl << "Isi tree : " << endl;
-    print();
+    node_3 = insertRight(3, root);
+    node_4 = insertLeft(4, node_2);
+    node_5 = insertRight(5, node_2);
+    node_6 = insertRight(6, node_3);
+    node_7 = insertRight(7, node_5);
+    node_8 = insertLeft(8, node_6);
+    node_9 = insertRight(9, node_6);
+
+    cout << endl << "Traversal Tree:" << endl;
+
+    cout << "Pre-Order: ";
+    preOrder(root);
+    cout << endl;
+
+    cout << "In-Order: ";
+    inOrder(root);
+    cout << endl;
+
+    cout << "Post-Order: ";
+    postOrder(root);
+    cout << endl;
+
+    deleteTree();
+    return 0;
 }
